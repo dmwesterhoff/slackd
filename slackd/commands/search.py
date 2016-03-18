ec87abc693d65fcedb2233b97f84b604c37b5930
David Westerhoff <dmwesterhoff@gmail.com>
2016-03-18 14:30:30 -0700

    Search methods stubbed out

--- slackd/commands/search.py
+++ slackd/commands/search.py
@@ -8,6 +8,7 @@ slackd.commands.search
 """
 
 import click
+from common.slack_client import slack_client
 
 
 @click.group(invoke_without_command=True)
@@ -16,17 +17,35 @@ def search(ctx):
     """Searches slack for both files and messages, or if supplied with a
     subcommand will filter to one or the other."""
     if ctx.invoked_subcommand is None:
-        click.secho('search')
+        try:
+            response = slack_client.search.all()
+            if response.successful:
+                data = response.body
+                print data
+        except Exception as e:
+            print str(e)
 
 
 @click.command()
 def messages():
-    click.secho('messages only')
+    try:
+        response = slack_client.search.messages()
+        if response.successful:
+            data = response.body
+            print data
+    except Exception as e:
+        print str(e)
 
 
 @click.command()
 def files():
-    click.secho('files only')
+    try:
+        response = slack_client.search.files()
+        if response.successful:
+            data = response.body
+            print data
+    except Exception as e:
+        print str(e)
 
 
 search.add_command(messages)
